250cdf3e9e99a8f195f8ac1b5b56b5be199ca689
matthewjgarcia <35940299+matthewjgarcia@users.noreply.github.com>
2019-04-16 21:52:08 -0700

    Add files via upload

---
+++ book.cpp
@@ -0,0 +1,27 @@
+#include "book.h"
+
+Book::Book(int id, string bookName, string auth, string cat)
+  : bookID(id), title(bookName), author(auth), category(cat) {}
+
+string Book::getTitle() {
+    return title;
+}
+
+string Book::getAuthor() {
+    return author;
+}
+
+string Book::getCategory() {
+    return category;
+}
+
+int Book::getId() {
+    return bookID;
+}
+void Book::setPersonPtr(Person * ptr) {
+  personPtr = ptr;
+}
+
+Person * Book::getPersonPtr() {
+    return personPtr;
+}
---
+++ person.cpp
@@ -0,0 +1,29 @@
+#include "person.h"
+
+Person::Person(int cardNo, bool act, string fName, string lName)
+    : cardID(cardNo), active(act), firstName(fName), lastName(lName) {}
+
+
+string Person::getFirstName() {
+    return firstName;
+}
+
+string Person::getLastName() {
+    return lastName; // complete
+}
+
+int Person::getId() {
+    return cardID; // complete
+}
+
+void Person::setActive(bool act) {
+  active = act;
+} // complete
+
+bool Person::isActive() {
+    return active; // complete
+}
+
+string Person::fullName() {
+    return firstName + " " + lastName; // complete
+}
